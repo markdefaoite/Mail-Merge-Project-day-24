de027f9c914328af89579e73a9b4c3211a6bc107
Mark White <markdefaoite@hotmail.com>
2024-05-31 11:10:13 -0400

    adding completed output files

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -6,3 +6,18 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER = "[name]"
+
+with open("./input/names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+
+with open("./input/letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+
+        with open(f"./output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
